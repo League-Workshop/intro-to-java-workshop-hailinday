fe1f6b261e7532cead7d246b0ba2a7909a7e8b8c
League Student <league@iMac.attlocal.net>
2019-09-09 18:30:09 -0700

    Working on section 4 if statements and JOptionPane commands.

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,9 +2,37 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	int number = new Random().nextInt(4);
+	System.out.print(number);
+	JOptionPane.showInputDialog("What do you think is awesome");
+	if (number == 0) {
+		JOptionPane.showMessageDialog(null,"What you entered is awesome");
+	} 
+	
+
+	if (number == 1) {
+		JOptionPane.showMessageDialog(null,"What you entered is ok");
+	}
+	if (number == 2) {
+		JOptionPane.showMessageDialog(null,"What you entered is boring");
+
+	}
+	if (number == 3) {
+		JOptionPane.showMessageDialog(null,"What you entered is wierd");
+	}
+	
+
+	
+
+ 
+}
+
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,20 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String str = JOptionPane.showInputDialog("What is 2+2");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (str.equals("4")) {
+		score += 1;	
+		}
+		JOptionPane.showMessageDialog(null,"score: "+ score);
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
